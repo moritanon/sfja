f18cd5ddbcedcd6558d6c2970004f9cff494c5c4
morita.non <morita.non@gmail.com>
2014-07-15 13:54:36 +0900

    古いlistリテラル表記を修正

--- Poly_J.v
+++ Poly_J.v
@@ -921,7 +921,7 @@ Fixpoint flat_map {X Y:Type} (f:X -> list Y) (l:list X)
   (* FILL IN HERE *) admit.
 
 Example test_flat_map1:
-  flat_map (fun n => [n,n,n]) [1,5,4]
+  flat_map (fun n => [n;n;n]) [1;5;4]
   = [1; 1; 1; 5; 5; 5; 4; 4; 4].
  (* FILL IN HERE *) Admitted.
 (** [] *)
