55a42fea68bb9ee1387c9e5defc20f91777b5ed8
moritanon <morita.non@gmail.com>
2017-05-28 00:52:48 +0900

    ちょっと修正

--- Poly_J.v
+++ Poly_J.v
@@ -1,12 +1,12 @@
 (*  * Poly: Polymorphism and Higher-Order Functions *)
 (** * Poly_J:多相性と高階関数 *)
 
-(* REMINDER: Please do not put solutions to the exercises in
+(* Final reminder: Please do not put solutions to the exercises in
    publicly accessible places.  Thank you!! *)
-(* REMINDER: どうか練習問題の答を公にアクセス出来る場所に置かないで。ありがとう *)
+(* Final reminder: どうか練習問題の答を公にアクセス出来る場所に置かないで。ありがとう *)
 Require Export Lists.
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (*  * Polymorphism *)
 (** * ポリモルフィズム（多相性） *)
 
@@ -19,7 +19,7 @@ Require Export Lists.
 *)
 
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (*  ** Polymorphic Lists *)
 (** ** 多相的なリスト *)
 
@@ -29,7 +29,10 @@ Require Export Lists.
     lists of strings, lists of booleans, lists of lists, etc.  We
     _could_ just define a new inductive datatype for each of these,
     for example... *)
-(** ここまでは、数値のリストについて学んできました。もちろん、数値以外の、文字列、ブール値、リストといったものを要素として持つリストを扱えるプログラムは、より興味深いものとなるでしょう。これまで学んできたことだけでも、実は新しい帰納的なデータ型を作ることはできるのです。例えば次のように... *)
+(** ここまでは、数値のリストについて学んできました。もちろん、数値以外の文字列、
+ブール値、リストといったものを要素として持つリストを扱えるプログラムは、
+より興味深いものとなるでしょう。これまで学んできたことだけでも、
+実は新しい帰納的なデータ型を作ることはできるのです。例えば次のように... *)
 
 Inductive boollist : Type :=
   | bool_nil : boollist
@@ -38,7 +41,7 @@ Inductive boollist : Type :=
 (*  ... but this would quickly become tedious, partly because we
     have to make up different constructor names for each datatype, but
     mostly because we would also need to define new versions of all
-    our list manipulating functions ([length], [rev], etc.)  for each
+    our list manipulating functions ([length], [rev], etc.) for each
     new datatype definition. *)
 (** ... しかし、こんなことをやっていると、すぐに嫌になってしまうでしょう。その理由の一つは、データ型ごとに違ったコンストラクタの名前をつけなければならないことですが、もっと大変なのは、こういったリストを扱う関数（[length]、[rev]など）を、新しく対応した型ごとに作る必要が出てくることです。 *)
 
@@ -58,14 +61,14 @@ Inductive list (X:Type) : Type :=
     [natlist] in the types of the constructors have been replaced by
     [list X].  (We can re-use the constructor names [nil] and [cons]
     because the earlier definition of [natlist] was inside of a
-    [Module] definition that is now out of scope.) 
+    [Module] definition that is now out of scope.)
 
     What sort of thing is [list] itself?  One good way to think
     about it is that [list] is a _function_ from [Type]s to
     [Inductive] definitions; or, to put it another way, [list] is a
     function from [Type]s to [Type]s.  For any particular type [X],
     the type [list X] is an [Inductive]ly defined set of lists whose
-    elements are things of type [X]. *)
+    elements are of type [X]. *)
 (** これは、前の章にある[natlist]の定義とほとんどそっくりですが、コンストラクタ[cons]の引数の型が[nat]であったのに対し、任意の型を表す[X]がそれに変わっています。この[X]はヘッダに加えられた[X]と結びつけられ、さらに[natlist]であったものが[list X]に置き換わっています（ここで、コンストラクタに[nil]や[cons]といった名前を付けられるのは、最初に定義した[natlist]が[Module]の中で定義されていて、ここからはスコープの外になっているからです）。
  それでは、[list]とはいったい何なのか、ということを正確に考えて見ましょう。これを考える一つの手がかりは、リストが「型を引数にとり、帰納的な定義を返す関数である」と考えることです。あるいは「型を引数にとり、型を返す関数」と考えてもいいかもしれません。任意の型[X]について、[list X]という型は、帰納的に定義されたリストの集合で、その要素の型が[X]となっているものと考えることもできます。
  *)
@@ -82,13 +85,17 @@ Check nil.
 Check cons.
 (* ===> cons : forall X : Type, X -> list X -> list X *)
 
-(** (Side note on notation: In .v files, the "forall" quantifier is
-    spelled out in letters.  In the generated HTML files, [forall] is
-    usually typeset as the usual mathematical "upside down A," but
-    you'll see the spelled-out "forall" in a few places, as in the
-    above comments.  This is just a quirk of typesetting: there is no
-    difference in meaning.) *)
-(** 記法の点から: .vファイル内で、"forall"量化子は文字で綴られてます。生成されたHTMLファイルにおいて、[forall]は通常、数学の"ひっくりかえったA"が見えていると思います。しかし、上記のコメントなど、少数の場所では"forall"がそのまま見えているかもしれません。これは植字のよじれに過ぎません。意味に違いはありません *)
+(*  (Side note on notation: In .v files, the "forall" quantifier
+    is spelled out in letters.  In the generated HTML files and in the
+    way various IDEs show .v files (with certain settings of their
+    display controls), [forall] is usually typeset as the usual
+    mathematical "upside down A," but you'll still see the spelled-out
+    "forall" in a few places.  This is just a quirk of typesetting:
+    there is no difference in meaning.) *)
+(** 記法の点から: .vファイル内で、"forall"量化子は文字で綴られてます。生成されたHTMLファイル内やいくつかの
+    IDEの.vファイルの表示(表示設定がそうなっている場合にですが)において、 [forall]は通常、
+    数学の"ひっくりかえったA"が見えていると思います。しかし、上記のコメントなど、少数の場所では"forall"がそのまま見えているかもしれません。
+    これは植字のよじれに過ぎません。意味に違いはありません *)
 
 (*  The "[forall X]" in these types can be read as an additional
     argument to the constructors that determines the expected types of
@@ -170,62 +177,7 @@ Inductive grumble (X:Type) : Type :=
 
 End MumbleGrumble.
 
-(** 次の式のうち、ある型[X]について[grumble X]の要素として正しく定義されているものはどれでしょうか。
-- [d (b a 5)]
-- [d mumble (b a 5)]
-- [d bool (b a 5)]
-(* Note that the uses of [nil] and [cons] in [match] patterns
-    do not require any type annotations: Coq already knows that the list
-    [l] contains elements of type [X], so there's no reason to include
-    [X] in the pattern.  (More precisely, the type [X] is a parameter
-    of the whole definition of [list], not of the individual
-    constructors.  We'll come back to this point later.)
-
-    As with [nil] and [cons], we can use [length] by applying it first
-    to a type and then to its list argument: *)
-(** ここで、[match]に使われている[nil]や[cons]に型修飾がついていないことに注意してください。我々はすでにリスト[l]が型[X]の要素を持っていることを知っていますが、それはパターンマッチに[X]を含める理由になりません。(さらに正確には、[X]はリスト定義全体での型であって、個々のコンストラクタの型ではないのです。この点に関しては、後で戻ってきます。)
-
-    [nil]や[cons]と同様に、[length]も与えるリストと同じ型を最初に適用すれば使えます。 *)
-
-Example test_length1 :
-    length nat (cons nat 1 (cons nat 2 (nil nat))) = 2.
-Proof. reflexivity.  Qed.
-
-(* To use our length with other kinds of lists, we simply
-    instantiate it with an appropriate type parameter: *)
-(** この[length]を別の型のリストに使いたい場合は、適切な型パラメータを与えるだけで済みます。 *)
-
-Example test_length2 :
-    length bool (cons bool true (nil bool)) = 1.
-Proof. reflexivity.  Qed.
-
-
-(** *** *)
-
-
-- [e bool true]
-- [e mumble (b c 0)]
-- [e bool (b c 0)]
-- [c]
-(** FILL IN HERE *)
-*)
-(** [] *)
-
-
-(* **** Exercise: 2 stars (baz_num_elts)  *)
-(* Consider the following inductive definition: *)
-(** **** 練習問題: ★★, (baz_num_elts) *)
-(** 次の帰納的定義について考えなさい *)
-
-Inductive baz : Type :=
-   | x : baz -> baz
-   | y : baz -> bool -> baz.
-
-(* How _many_ elements does the type [baz] have? *)
-(** 型[baz]はいくつの要素を持つことができるでしょうか？
-(* FILL IN HERE *)
-*)
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (*  *** Type Annotation Inference *)
 (** *** 型推論 *)
 
@@ -265,16 +217,25 @@ Check repeat.
     few (which forces readers to perform type inference in their heads
     in order to understand your code). *)
 
-(** 両者は[repeat]として全く同じ型であることが分かります。Coqは型推論という機構を持っていて、これを使い、[X]と[x]と[count]の型が何であるべきかを、その使われ方から導き出します。例えば、[X]が[cons]の引数として使われたことがあれば、それは型に違いなく、さらに[cons]の最初に引数で型が指定されていて、[l1]が[nil]や[cons]とマッチされているなら、それは[list]に違いなかろう、といった具合です。
+(** 両者は[repeat]として全く同じ型であることが分かります。Coqは型推論という機構を持っていて、
+    これを使い、[X]と[x]と[count]の型が何であるべきかを、その使われ方から導き出します。
+    例えば、[X]が[cons]の引数として使われたことがあれば、それは型に違いなく、
+    さらに[cons]の最初に引数で型が指定されていて、[l1]が[nil]や[cons]とマッチされているなら、
+    それは[list]に違いなかろう、といった具合です。
 
-    このパワフルな機構の存在は、型情報を常にそこら中に書かなければならないわけではない、ということを意味します。とはいえ、型を明示的に書くことは、ドキュメント作成やプログラムの健全性をチェックする際に大いに意味はあるのですが。とにかく、これからは自分の書くコードで、型指定を書くところ、書かないところのバランスを考えていく必要があります（多すぎれば、コードが目障りな型指定で読みにくくなりますし、少なすぎると、プログラムを読む人に型推論の結果をいちいち推測させなければならなくなります）。
+    このパワフルな機構の存在は、型情報を常にそこら中に書かなければならないわけではない、
+    ということを意味します。とはいえ、型を明示的に書くことは、ドキュメント作成やプログラムの
+    健全性をチェックする際に大いに意味はあるのですが。とにかく、これからは自分の書くコードで、
+    型指定を書くところ、書かないところのバランスを考えていく必要があります
+    （多すぎれば、コードが目障りな型指定で読みにくくなりますし、
+    少なすぎると、プログラムを読む人に型推論の結果をいちいち推測させなければならなくなります）。
 *)
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (*  *** Type Argument Synthesis *)
 (** *** 引数の同化（Synthesis） *)
 
-(*  To we use a polymorphic function, we need to pass it one or
+(*  To use a polymorphic function, we need to pass it one or
     more types in addition to its other arguments.  For example, the
     recursive call in the body of the [repeat] function above must
     pass along the type [X].  But since the second argument to
@@ -292,7 +253,7 @@ Check repeat.
     application appears -- to determine what concrete type should
     replace the [_].
 
-    This may sound similar to type annotation inference -- indeed,
+    This may sound similar to type annotation inference -- indeed, the
     two procedures rely on the same underlying mechanisms.  Instead of
     simply omitting the types of some arguments to a function, like
 
@@ -304,8 +265,8 @@ Check repeat.
 
     to tell Coq to attempt to infer the missing information.
 
-    Using implicit arguments, the [count] function can be written
-    like this: *)
+    Using implicit arguments, the [count] function can be written like
+    this: *)
 (** 多相的な関数を使うためには、通常の引数に加えて型を一つ以上渡さなければなりません。例えば、[repeat]関数で自分を再帰している部分には、型[X]として渡されたものをそのまま渡すことになります。しかし[repeat]の二番目の引数は[X]の要素になり、最初の引数は、[X]以外になり得ないのはあきらかに見えます。・・・ならなぜわざわざそれを書く必要があるのでしょう。
 
     幸いなことに、Coqではこの種の冗長性を避けることができます。型を引数に渡すところではどこでも同化した引数"[_]"を書くことができるのです。これは「ここをあるべき型に解釈してください」という意味です。もう少し正確に言うと、Coqが[_]を見つけると、手近に集められる情報を集めます。それは、適用されている関数の型や、その適用が現れている文脈から予想される型などですが、それを使って[_]と置き換えるべき具体的な型を決定するのです。
@@ -339,7 +300,7 @@ Definition list123 :=
 Definition list123' :=
   cons _ 1 (cons _ 2 (cons _ 3 (nil _))).
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (*  *** Implicit Arguments *)
 (** *** 暗黙の引数 *)
 
@@ -432,6 +393,10 @@ Proof. reflexivity.  Qed.
 Example test_length1: length (cons 1 (cons 2 (cons 3 nil))) = 3.
 Proof. reflexivity.  Qed.
 
+(* ----------------------------------------------------------------- *)
+(*  *** Supplying Type Arguments Explicitly *)
+(** *** 明示的に型引数を与えること *)
+
 (*  One small problem with declaring arguments [Implicit] is
     that, occasionally, Coq does not have enough local information to
     determine a type argument; in such cases, we need to tell Coq that
@@ -483,100 +448,12 @@ Notation "x ++ y" := (app x y)
 
 Definition list123''' := [1; 2; 3].
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (*  *** Exercises *)
 (** *** 練習問題 *)
 
 (*  **** Exercise: 2 stars, optional (poly_exercises) *)
-(** **** 練習問題: ★★, optional (poly_exercises) *)
-(*  Here are a few simple exercises, just like ones in the [Lists]
-    chapter, for practice with polymorphism.  Complete the proofs below. *)
-(** ここにあるいくつかの練習問題は、List_J.vにあったものと同じですが、多相性の練習になります。以下の証明を完成させなさい。 *)
-
-Theorem app_nil_r : forall (X:Type), forall l:list X,
-  l ++ [] = l.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem app_assoc : forall A (l m n:list A),
-  l ++ m ++ n = (l ++ m) ++ n.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Lemma app_length : forall (X:Type) (l1 l2 : list X),
-  length (l1 ++ l2) = length l1 + length l2.
-Proof.
-  (* FILL IN HERE *) Admitted.
-(** [] *)
-
-(** **** Exercise: 2 stars, optional (more_poly_exercises)  *)
-(*  **** 練習問題: ★★, optional (more_poly_exercises) *)
-(*  Here are some slightly more interesting ones... *)
-(** もっと興味深い定理があります... *)
-
-Theorem rev_app_distr: forall X (l1 l2 : list X),
-  rev (l1 ++ l2) = rev l2 ++ rev l1.
-Proof.
-  (* FILL IN HERE *) Admitted.
-
-Theorem rev_involutive : forall X : Type, forall l : list X,
-  rev (rev l) = l.
-Proof.
-  (* FILL IN HERE *) Admitted.
-(** [] *)
-
-(* ###################################################### *)
-(*  ** Polymorphic Pairs *)
-(** ** 多相的なペア *)
-
-(** Following the same pattern, the type definition we gave in
-    the last chapter for pairs of numbers can be generalized to
-    _polymorphic pairs_ (or _products_): *)
-(** 次も同様に、前の章で作った数値のペア(良くプロダクト(積)とも呼ばれます)を多相的にすることを考えます。 *)
-
-Inductive prod (X Y : Type) : Type :=
-| pair : X -> Y -> prod X Y.
-
-Arguments pair {X} {Y} _ _.
-
-(*  As with lists, we make the type arguments implicit and define the
-    familiar concrete notation. *)
-(** リストと同様、型引数を暗黙にし、具体的な表記法を定義します。 *)
-
-Notation "( x , y )" := (pair x y).
-
-(*  We can also use the [Notation] mechanism to define the standard
-    notation for product _types_: *)
-(** また、_型_の掛け合わせとしての、より標準的な表記法も登録しておきます。 *)
-
-Notation "X * Y" := (prod X Y) : type_scope.
-
-(*  (The annotation [: type_scope] tells Coq that this abbreviation
-    should only be used when parsing types.  This avoids a clash with
-    the multiplication symbol.) *)
-(** （[type_scope]というアノテーションは、この省略形が、型を解析する際にのみ使われるものであることを示しています。これによって、[*]が乗算の演算子と衝突することを避けています。*)
-
-(*  It is easy at first to get [(x,y)] and [X*Y] confused.
-    Remember that [(x,y)] is a _value_ built from two other values,
-    while [X*Y] is a _type_ built from two other types.  If [x] has
-    type [X] and [y] has type [Y], then [(x,y)] has type [X*Y]. *)
-(** 最初は、[(x,y)]と[X*Y]がこんがらがりやすいかもしれません。[(x,y)]は二つの別の値から生成された_値_であり、[X*Y]は、二つの別の型から生成される_型_であることを忘れないでいてください。もし[x]が型[X]を持っており[y]が型[Y]を持っているならば、[(x,y)]は、型[X*Y]を持ちます。*)
-
-(*  The first and second projection functions now look pretty
-    much as they would in any functional programming language. *)
-(** ペアの最初の要素、２番目の要素を返す射影関数は他のプログラミング言語で書いたものと比べると若干長くなります。 *)
-
-Definition fst {X Y : Type} (p : X * Y) : X :=
-  match p with
-  | (x, y) => x
-  end.
-
-Definition snd {X Y : Type} (p : X * Y) : Y :=
-  match p with
-  | (x, y) => y
-  end.
-
-(* The following function takes two lists and combines them
+(** **** 練習問題: ★★, optional (poly_exercises) *combines them
     into a list of pairs.  In other functional languages, it is often
     called [zip]; we call it [combine] for consistency with Coq's
     standard library. *)
@@ -630,7 +507,7 @@ Proof.
 (* FILL IN HERE *) Admitted.
 (** [] *)
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (* ** Polymorphic Options *)
 (** ** 多相的オプション *)
 
@@ -685,7 +562,7 @@ Example test_hd_error2 : hd_error  [[1];[2]]  = Some [1].
  (* FILL IN HERE *) Admitted.
 (** [] *)
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (* * Functions as Data *)
 (** * データとしての関数 *)
 
@@ -696,7 +573,7 @@ Example test_hd_error2 : hd_error  [[1];[2]]  = Some [1].
     results, stored in data structures, etc.*)
 (** 他の多くの近代的なプログラミング言語 --全ての関数型言語(ML, Haskell, Scheme, Scala, Clojureなど) を含む -- と同様、Coqは関数をファーストクラスに属するものとして取り扱います。つまりそれは、関数を他の関数の引数として渡したり、結果として関数を返したり、データ構造の中に関数を含めたりすることができるということです。*)
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (* ** Higher-Order Functions *)
 (** ** 高階関数 *)
 
@@ -722,7 +599,7 @@ Proof. reflexivity.  Qed.
 Example test_doit3times': doit3times negb true = false.
 Proof. reflexivity.  Qed.
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (** ** Filter *)
 (** ** フィルター (Filter)関数 *)
 
@@ -771,7 +648,7 @@ Proof. reflexivity.  Qed.
 Example test_countoddmembers'3:   countoddmembers' nil = 0.
 Proof. reflexivity.  Qed.
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (** ** 無名（匿名）関数 *)
 
 (*  It is arguably a little sad, in the example just above, to
@@ -854,7 +731,7 @@ Example test_partition2: partition (fun x => false) [5;9;0] = ([], [5;9;0]).
 (* FILL IN HERE *) Admitted.
 (** [] *)
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (*  ** Map *)
 (** ** マップ（Map）関数 *)
 
@@ -958,7 +835,7 @@ Definition option_map {X Y : Type} (f : X -> Y) (xo : option X)
     throw away afterwards.)  [] *)
 (** [filter]や[map]関数を定義したり使ったりするケースでは、多くの場合暗黙的な型引数が使われます。暗黙の型引数定義に使われている中括弧を普通の括弧に置き換え、必要なところに型引数を明示的に書くようにして、それが正しいかどうかをCoqでチェックしなさい。 [] *)
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (*  ** Fold *)
 (** ** 畳み込み（Fold）関数 *)
 
@@ -1024,7 +901,7 @@ Proof. reflexivity. Qed.
     different? *)
 (** [fold]関数が[X]と[Y]二つの型引数をとっていて、関数[f]が型[X]を引数にとり型[Y]を返すようになっていることに注目してください。[X]と[Y]が別々の型となっていることで、どのような場合に便利であるかを考えてください。 *)
 
-(* ###################################################### *)
+(* ----------------------------------------------------------------- *)
 (*  ** Functions That Construct Functions *)
 (** ** 関数を作成する関数 *)
 
@@ -1078,7 +955,7 @@ Proof. reflexivity.  Qed.
 Example test_plus3'' :  doit3times (plus 3) 0 = 9.
 Proof. reflexivity.  Qed.
 
-(* ##################################################### *)
+(* ----------------------------------------------------------------- *)
 (*  * Additional Exercises *)
 (** * さらなる練習問題 *)
 
